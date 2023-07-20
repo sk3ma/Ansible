59d298dc7d87409e5fb6560f5ba12b8c03a41893
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-07-20 10:07:06 +0200

    Update AWX.sh

--- AWX.sh
+++ AWX.sh
@@ -25,27 +25,27 @@ STOP
 
 # Package installation.
 system() {
-    echo -e "\e[96;1;3m[ INFO ] Distribution: ${DISTRO}\e[m"
+    echo -e "\e[96;1;3m[INFO] Distribution: ${DISTRO}\e[m"
     sudo apt update
-    echo -e "\e[32;1;3m[ INFO ] Installing packages\e[m"
+    echo -e "\e[32;1;3m[INFO] Installing packages\e[m"
     sudo apt install nodejs npm python3-pip git -y
     sudo npm install npm --global
 }
 
 # Docker installation.
-container() {
-    echo -e "\e[32;1;3m[ INFO ] Installing packages\e[m"
+install() {
+    echo -e "\e[32;1;3m[INFO] Installing packages\e[m"
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     sudo bash -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -'
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-    echo -e "\e[32;1;3m[ INFO ] Installing Docker\e[m"
+    echo -e "\e[32;1;3m[INFO] Installing Docker\e[m"
     sudo apt install docker-ce docker-compose -y
     pip3 install docker-compose==1.25.0
 }
 
 # Ansible installation.
 awx() {
-    echo -e "\e[32;1;3m[ INFO ] Installing Ansible\e[m"
+    echo -e "\e[32;1;3m[INFO] Installing Ansible\e[m"
     cd /tmp
     sudo apt install pwgen unzip ansible -y
     local secret=$(pwgen -N 1 -s 40)
@@ -72,16 +72,16 @@ admin_password=1q2w3e4r5t
 secret_key=${secret}
 create_preload_data=True
 STOP
-    echo -e "\e[32;1;3m[ INFO ] Executing playbook\e[m"
+    echo -e "\e[32;1;3m[INFO] Executing playbook\e[m"
     sudo ansible-playbook -i inventory install.yml
-    echo -e "\e[33;1;3;5m[ INFO ] Finished, installation complete.\e[m"
+    echo -e "\e[33;1;3;5m[INFO] Finished, installation complete.\e[m"
     exit
 }
 
 # Calling functions.
 if [[ -f /etc/lsb-release ]]; then
-    echo -e "\e[35;1;3;5m[ INFO ] Ubuntu detected, proceeding...\e[m"
+    echo -e "\e[35;1;3;5m[INFO] Ubuntu detected, proceeding...\e[m"
     system
-    container
+    install
     awx
 fi
